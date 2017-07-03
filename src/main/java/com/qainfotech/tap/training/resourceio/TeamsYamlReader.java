6045b5b9773c7771c8bd0a4b35aaaaa90d57fec5
Vibhuti Gupta <QAIT\vibhutigupta@asda1716po02925.qait.com>
2017-07-03 12:46:11 +0530

    yamlfile

---
+++ src/main/java/com/qainfotech/tap/training/resourceio/TeamsYamlReader.java
@@ -0,0 +1,73 @@
+package com.qainfotech.tap.training.resourceio;
+
+import com.qainfotech.tap.training.resourceio.exceptions.ObjectNotFoundException;
+import com.qainfotech.tap.training.resourceio.model.Individual;
+import com.qainfotech.tap.training.resourceio.model.Team;
+import java.util.List;
+
+
+/**
+ *
+ * @author Ramandeep RamandeepSingh AT QAInfoTech.com
+ */
+public class TeamsYamlReader{
+    
+    /**
+     * get a list of individual objects from db yaml file
+     * 
+     * @return 
+     */
+    public List<Individual> getListOfIndividuals(){
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+    
+    /**
+     * get individual object by id
+     * 
+     * @param id individual id
+     * @return 
+     * @throws com.qainfotech.tap.training.resourceio.exceptions.ObjectNotFoundException 
+     */
+    public Individual getIndividualById(Integer id) throws ObjectNotFoundException{
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+    
+    /**
+     * get individual object by name
+     * 
+     * @param name
+     * @return 
+     * @throws com.qainfotech.tap.training.resourceio.exceptions.ObjectNotFoundException 
+     */
+    public Individual getIndividualByName(String name) throws ObjectNotFoundException{
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+    
+    
+    /**
+     * get a list of individual objects who are not active
+     * 
+     * @return List of inactive individuals object
+     */
+    public List<Individual> getListOfInactiveIndividuals(){
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+    
+    /**
+     * get a list of individual objects who are active
+     * 
+     * @return List of active individuals object
+     */
+    public List<Individual> getListOfActiveIndividuals(){
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+    
+    /**
+     * get a list of team objects from db yaml
+     * 
+     * @return 
+     */
+    public List<Team> getListOfTeams(){
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+}
